61feedd6fc1174c9fd7ce13ed35cfd3d9b22150d
Adnan Gondal <adnangondal1@hotmail.com>
2021-09-16 19:03:33 +0100

    post ticket done

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,21 +1,31 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import Blog from "./Blog";
+import Post from "./Post";
+import { userContext } from "../App";
 
 function Main() {
   const [allBlogs, setAllBlogs] = useState(null);
+  const { user, setUser } = useContext(userContext);
+  const [count, setCount] = useState(0);
+
+  async function fetchAllBlogs() {
+    const url = "https://sigma-micro-blogging.herokuapp.com/";
+    let res = await fetch(url);
+    res = await res.json();
+    setAllBlogs(res.messages);
+  }
 
   useEffect(() => {
-    async function fetchAllBlogs() {
-      const url = "https://sigma-micro-blogging.herokuapp.com/";
-      let res = await fetch(url);
-      res = await res.json();
-      setAllBlogs(res.messages);
-    }
-
     fetchAllBlogs();
+    setInterval(() => {
+      fetchAllBlogs();
+    }, 5000);
   }, []);
 
+  //useEffect(() => {}, [allBlogs]);
+
   const showBlogs = (blogs) => {
+    console.log(count);
     return blogs.map((blog) => {
       return <Blog key={blog.id} blog={blog} />;
     });
@@ -23,6 +33,7 @@ function Main() {
 
   return (
     <main>
+      {user && <Post user={user} update={fetchAllBlogs} />}
       <h2>Blog Posts</h2>
       {allBlogs && showBlogs(allBlogs)}
     </main>
---
+++ src/Components/Post.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+function Post(props) {
+  const [text, setText] = useState("");
+
+  const handleChange = (evt) => {
+    setText(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    async function postBlog() {
+      const data = { message: text, from: props.user };
+      console.log(data);
+      let res = await fetch(
+        "https://sigma-micro-blogging.herokuapp.com/SigmaStudent99",
+        { method: "POST", body: JSON.stringify(data) }
+      );
+      res = await res.json();
+      console.log(res);
+    }
+    postBlog();
+    evt.preventDefault();
+    props.update();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>What are you thinking about?</label>
+        <textarea onChange={handleChange} value={text}></textarea>
+        <button> Post </button>
+      </form>
+    </div>
+  );
+}
+
+export default Post;
